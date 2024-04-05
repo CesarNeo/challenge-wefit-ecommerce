df39f4868e34e535acd14583d25979c0be959895
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-04-05 14:10:01 -0300

    feat: configure react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 import { GlobalStyles } from './styles/global'
 import { DEFAULT_THEME } from './styles/themes/default'
 
 function App() {
   return (
-    <ThemeProvider theme={DEFAULT_THEME}>
-      <RouterProvider router={router} />
-      <GlobalStyles />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={DEFAULT_THEME}>
+        <RouterProvider router={router} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
